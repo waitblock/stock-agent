e399b3de37e8d3470e69fb1f89dd2a32158b81bc
Ethan <65051086+waitblock@users.noreply.github.com>
2026-05-30 15:47:08 -0400

    Update subject line and prompt

--- send.py
+++ send.py
@@ -1,9 +1,10 @@
 import smtplib
 from email.message import EmailMessage
+from datetime import date
 
 def send_email(content, sender, pw, receiver, fallback="Your email client does not support HTML."):
 	msg = EmailMessage()
-	msg["Subject"] = "Testing"
+	msg["Subject"] = f"Stock Report {date.today()}"
 	msg["From"] = sender
 	msg["To"] = receiver
 
