0a249853a75a758618f3191ca55aecf983180597
Ethan <65051086+waitblock@users.noreply.github.com>
2026-05-30 15:35:00 -0400

    Update HTML format

--- parse.py
+++ parse.py
@@ -10,7 +10,6 @@ def generate_html(data):
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Markdown Render</title>
 		<style>
 		    body {{
 		    	font-size: 18px;
